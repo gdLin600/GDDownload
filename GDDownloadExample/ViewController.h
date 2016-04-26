eef034924b34a4251ccecee2483a7265d406246e
lingd600 <gd@linguangdedeMac.local>
2016-04-26 16:27:34 +0800

    初始化项目

---
+++ GDDownloadExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GDDownloadExample
+//
+//  Created by 林广德 on 16/4/26.
+//  Copyright © 2016年 林广德. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
